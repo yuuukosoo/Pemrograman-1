a81e68e6de69f7b81bf81b101168a0e7d1659c6d
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-10-19 16:39:05 +0700

    Delete Modul-1 directory

--- Modul-1/Soal No 1 Praktikum Pemrograman  1/PRAK101-2410817110004-MUHAMMADNAUFALKHALISH.py
+++
@@ -1,9 +0,0 @@
-x=5
-y=7
-z=9
-Jumlah=x+y+z
-
-print("Variabel x bernilai %d"%(x))
-print("Variabel y bernilai %d"%(y))
-print("Variabel z bernilai %d"%(z))
-print("Jumlah variabel tersebut adalah %d"%(Jumlah))
