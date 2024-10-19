4eba1f742f79f0e4f200a1ffae8fb1cf1c7211e6
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-10-19 16:28:39 +0700

    Add files via upload

---
+++ Modul-1/Soal No 1 Praktikum Pemrograman  1/PRAK101-2410817110004-MUHAMMADNAUFALKHALISH.py
@@ -0,0 +1,9 @@
+x=5
+y=7
+z=9
+Jumlah=x+y+z
+
+print("Variabel x bernilai %d"%(x))
+print("Variabel y bernilai %d"%(y))
+print("Variabel z bernilai %d"%(z))
+print("Jumlah variabel tersebut adalah %d"%(Jumlah))
---
+++ Modul-1/Soal No 6 Praktikum Pemrograman 1/PRAK106-2410817110004-MUHAMMADNAUFALKHALISH.py
@@ -0,0 +1,13 @@
+a=4
+b=8
+c=3
+
+print("Variabel a bernilai %d"%(a))
+print("Variabel b bernilai %d"%(b))
+print("Variabel c bernilai %d"%(c))
+
+print("Apakah a sama dengan b ? jawabannya adalah %d"%(a==b))
+print("Apakah b lebih besar dari c ? jawabannya adalah %d"%(b>c))
+print("Apakah a tidak sama dengan c ? jawabannya adalah %d"%(a!=c))
+         
+        
