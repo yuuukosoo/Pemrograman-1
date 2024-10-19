e15c4c5e68aa4fd203c41d05497d9b4edefee1d1
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-10-19 16:36:38 +0700

    Add files via upload

---
+++ Modul-1/Soal No 5 Praktikum Pemrograman 1/PRAK105-2410817110004-MUHAMMADNAUFALKHALISH.py
@@ -0,0 +1,11 @@
+a=9
+b=5
+x=8
+y=8
+Total=(a%b)+(x%y)
+
+print("Variabel a bernilai %d"%(a))
+print("Variabel b bernilai %d"%(b))
+print("Variabel x bernilai %d"%(x))
+print("Variabel y bernilai %d"%(y))
+print("Total sisa bagi dari a dibagi b dan x dibagi y adalah %d"% (Total))
