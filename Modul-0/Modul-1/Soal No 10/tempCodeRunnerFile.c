17a2b7d65b06d73d2c42825903dadb2c15a99054
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-10-21 09:57:09 +0700

    Delete Modul-0/Modul-1 directory

--- Modul-0/Modul-1/Soal No 10/tempCodeRunnerFile.c
+++
@@ -1,2 +0,0 @@
-
-    printf("Jawab :\n");
--- Modul-0/Modul-1/Soal No 2/PRAK102-2410817110004-MUHAMMADNAUFALKHALISH.py
+++
@@ -1,9 +0,0 @@
-a=4
-b=8
-c=3
-jumlah=(a*b)/c
-
-print("Variabel a bernilai %d"%(a))
-print("Variabel b bernilai %d"%(b))
-print("Variabel c bernilai %d"%(c))
-print("Hasil dari a dikali b dibagi c adalah %.6f"%(jumlah))
