29e76a2678fd29a78ca6036023294b248a591795
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-11-12 21:02:55 +0700

    Add files via upload

---
+++ Modul-3/Soal 2/PRAK302-2410817110004-MuhammadNaufalKhalish.py
@@ -0,0 +1,7 @@
+nilai=input()
+nilai=int(nilai)
+if nilai>=80 and nilai<=100 : print('A')
+elif nilai>=70 and nilai<=79 : print('B')
+elif nilai>=60 and nilai<=69 : print('C')
+elif nilai>=50 and nilai<=59 : print('D')
+else : print('E')
