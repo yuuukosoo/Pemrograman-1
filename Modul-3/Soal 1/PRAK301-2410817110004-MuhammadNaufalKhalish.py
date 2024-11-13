7817baa95b14d6052b76fe0ef86f4536278315ba
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-11-13 13:55:55 +0700

    Delete Modul-3 directory

--- Modul-3/Soal 1/PRAK301-2410817110004-MuhammadNaufalKhalish.py
+++
@@ -1,4 +0,0 @@
-a, b=input().split()
-a==int(a),b ==int(b)
-if a>b : print(f"{b} {a}")
-else : print(f"{a} {b}")
--- Modul-3/Soal 3/PRAK303-2410817110004-MuhammadNaufalKhalish.py
+++
@@ -1,5 +0,0 @@
-a=input()
-a=int(a)
-if a>0 : print("positif")
-elif a==0 : print("nol")
-else : print("negatif")
