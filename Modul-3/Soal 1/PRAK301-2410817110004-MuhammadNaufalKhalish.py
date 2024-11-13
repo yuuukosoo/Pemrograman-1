a309d2fc0c558d4929aeb03a538909db782cc7e8
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-11-13 13:56:17 +0700

    Add files via upload

---
+++ Modul-3/Soal 1/PRAK301-2410817110004-MuhammadNaufalKhalish.py
@@ -0,0 +1,10 @@
+a, b, c=map(int, input().split())
+
+if a>b>c : print(f"{c} {b} {a}")
+elif a>c>b : print(f"{b} {c} {a}")
+elif b>c>a : print(f"{a} {c} {b}")
+elif b>a>c : print(f"{c} {a} {b}")
+elif c>a>b : print(f"{b} {a} {c}")
+elif c>b>a : print(f"{a} {b} {c}")
+
+
