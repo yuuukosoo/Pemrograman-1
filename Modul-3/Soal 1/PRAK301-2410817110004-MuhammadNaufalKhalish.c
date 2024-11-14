0996c8c0871e7e702b4316fd0af74bad9027da38
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-11-14 21:21:21 +0700

    Add files via upload

---
+++ Modul-3/Soal 1/PRAK301-2410817110004-MuhammadNaufalKhalish.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+int main(){
+int a,b; scanf("%d %d",&a ,&b);
+if(b>a){printf("%d %d",a ,b);}
+else {printf("%d %d",b, a);}
+return 0;}
---
+++ Modul-3/Soal 1/PRAK301-2410817110004-MuhammadNaufalKhalish.py
@@ -0,0 +1,8 @@
+a, b=map(int, input().split()) 
+print(f"{b} {a}" if a>b  else(f"{a} {b}"))
+
+    
+
+
+
+
