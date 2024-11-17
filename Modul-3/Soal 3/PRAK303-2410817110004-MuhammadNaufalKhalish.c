2e9a3da705cc827db6951430169e9199ec2ddeb9
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-11-17 08:34:52 +0700

    Add files via upload

---
+++ Modul-3/Soal 3/PRAK303-2410817110004-MuhammadNaufalKhalish.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+int nilai;
+scanf("%d",&nilai);
+if(nilai>0){ printf("positif"); }
+else if(nilai==0){ printf("nol"); }
+else{printf("negatif"); } return 0;}
---
+++ Modul-3/Soal 3/PRAK303-2410817110004-MuhammadNaufalKhalish.py
@@ -0,0 +1,5 @@
+a=input()
+a=int(a)
+if a>0 : print("positif")
+elif a==0 : print("nol")
+else : print("negatif")
