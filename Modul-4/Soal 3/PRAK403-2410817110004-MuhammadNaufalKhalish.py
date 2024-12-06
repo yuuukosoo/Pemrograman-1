e17372d9e86e6d05ff5fbc070d9561e6eab6ab1b
yuuukosoo <muhammadnaufal.nf59@gmail.com>
2024-12-07 06:22:38 +0700

    Add files via upload

---
+++ Modul-4/Soal 3/PRAK403-2410817110004-MuhammadNaufalKhalish.py
@@ -0,0 +1,37 @@
+awal,akhir=map(int,input().split())
+if awal<=akhir:
+    i,j=awal,akhir
+    while i<=j :
+        print(f"{i} {j}", end=" ")
+        if i<=j :
+            print("-", end=" ")
+        i += 1
+        j -= 1
+
+    while i<=akhir:
+        print(f"{i} {j}", end=" ")
+        if i<akhir :
+            print("-", end=" ")
+        i += 1
+        j -= 1
+
+else :
+    i,j=awal,akhir
+    while i>=j:
+        print(f"{i} {j}",end=" ")
+        if i>=j :
+            print("-", end=" ")
+        i-=1
+        j+=1
+
+    while i>=akhir:
+        print(f"{i} {j}", end=" ")
+        if i>akhir :
+            print("-", end=" ")
+        i-=1
+        j+=1
+       
+
+
+
+
